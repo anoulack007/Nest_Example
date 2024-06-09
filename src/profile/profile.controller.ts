4a7123e8b0d7fe1e21f526c8d47bcccac0936fd3
Buek <anoulack1900@gmail.com>
2024-06-09 22:46:15 +0700

    commit

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -4,25 +4,26 @@ import { UpdateProfileDto } from './dto/update-profile.dto';
 import { request, Request } from 'express';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { FileFieldsInterceptor, FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
+import { ProfileService } from './profile.service';
 
 
 @UseGuards(JwtAuthGuard)
 @Controller('profile')
 export class ProfileController {
-  // constructor(private readonly profileService: ProfileService,
+  constructor(private readonly profileService: ProfileService,
 
-  // ) {}
+  ) {}
 
 
-  // @Get()
-  // findAll() {
-  //   return this.profileService.findAll();
-  // }
+  @Get()
+  findAll() {
+    return this.profileService.findAll();
+  }
 
-  // @Get('one')
-  // findOne(@Req() request:Request) {
-  //   return this.profileService.findOne(request);
-  // }
+  @Get('one')
+  findOne(@Req() request:Request) {
+    return this.profileService.findOne(request);
+  }
 
   /**
    * 
@@ -30,20 +31,20 @@ export class ProfileController {
    * @param updateProfileDto 
    * @param files 
    */
-  // @Patch()
-  // @UseInterceptors(FileFieldsInterceptor([
-  //   {name:'avatar'},
-  //   {name:'images'}
-  // ]))
-  // update(@Req() request:Request, @Body() updateProfileDto: UpdateProfileDto,@UploadedFiles() files:{avatar?:Express.Multer.File,images?:Express.Multer.File[]}) {
+  @Patch()
+  @UseInterceptors(FileFieldsInterceptor([
+    {name:'avatar'},
+    {name:'images'}
+  ]))
+  update(@Req() request:Request, @Body() updateProfileDto: UpdateProfileDto,@UploadedFiles() files:{avatar?:Express.Multer.File,images?:Express.Multer.File[]}) {
 
-  //   return this.profileService.update(request, updateProfileDto,files);
-  // }
+    return this.profileService.update(request, updateProfileDto,files);
+  }
 
-  // @Delete()
-  // remove(@Req() request:Request) {
-  //   return this.profileService.remove(request);
-  // }
+  @Delete()
+  remove(@Req() request:Request) {
+    return this.profileService.remove(request);
+  }
   
 
 
